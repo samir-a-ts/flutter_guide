1542b5a37389e2911d16105990bfc3cabf68f2df
lutrak <lutrak.developer@gmail.com>
2022-12-21 17:36:57 +0300

    feat(splash): splash page fully implemented (animation is strange, but whatever)

--- lib/src/presentation/core/app_widget.dart
+++ lib/src/presentation/core/app_widget.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_guide/src/core/constants/contants.dart';
 import 'package:flutter_guide/src/core/constants/theme.dart';
 import 'package:flutter_guide/src/core/router/app_router.gr.dart';
 import 'package:flutter_guide/src/core/translations/generated/l10n.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppWidget extends StatelessWidget {
   AppWidget({super.key});
@@ -12,21 +12,25 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: "Путеводитель",
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      routerDelegate: _appRouter.delegate(),
-      routeInformationProvider: _appRouter.routeInfoProvider(),
-      routeInformationParser: _appRouter.defaultRouteParser(),
-      localizationsDelegates: const [
-        AppTranslations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: AppTranslations.delegate.supportedLocales,
-      locale: const Locale("ru"),
+    return ScreenUtilInit(
+      designSize: const Size(360, 760),
+      builder: (context, child) => child!,
+      child: MaterialApp.router(
+        title: "Путеводитель",
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        routerDelegate: _appRouter.delegate(),
+        routeInformationProvider: _appRouter.routeInfoProvider(),
+        routeInformationParser: _appRouter.defaultRouteParser(),
+        localizationsDelegates: const [
+          AppTranslations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: AppTranslations.delegate.supportedLocales,
+        locale: const Locale("ru"),
+      ),
     );
   }
 }
