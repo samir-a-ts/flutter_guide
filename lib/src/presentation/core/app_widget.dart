93a55f31303cc2198f81615d26b4ebb31927fd38
lutrak <lutrak.developer@gmail.com>
2022-12-21 17:09:16 +0300

    feat(transpations): added intl to project generated files

--- lib/src/presentation/core/app_widget.dart
+++ lib/src/presentation/core/app_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_guide/src/core/constants/contants.dart';
 import 'package:flutter_guide/src/core/constants/theme.dart';
 import 'package:flutter_guide/src/core/router/app_router.gr.dart';
+import 'package:flutter_guide/src/core/translations/generated/l10n.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class AppWidget extends StatelessWidget {
   AppWidget({super.key});
@@ -11,12 +13,20 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: AppConstants.appName,
+      title: "Путеводитель",
       theme: lightTheme,
       darkTheme: darkTheme,
       routerDelegate: _appRouter.delegate(),
       routeInformationProvider: _appRouter.routeInfoProvider(),
       routeInformationParser: _appRouter.defaultRouteParser(),
+      localizationsDelegates: const [
+        AppTranslations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: AppTranslations.delegate.supportedLocales,
+      locale: const Locale("ru"),
     );
   }
 }
